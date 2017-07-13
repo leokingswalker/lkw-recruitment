c35e8fb35d65add83f003179d625b3d07f52aa4d
Simbadev <maxime.gasnier.1@gmail.com>
2017-07-13 16:19:44 +0200

    add the dancer class tests

--- lkw-recruitment/src/app/class/dancer.spec.ts
+++ lkw-recruitment/src/app/class/dancer.spec.ts
@@ -4,4 +4,19 @@ describe('Dancer', () => {
   it('should create an instance', () => {
     expect(new Dancer()).toBeTruthy();
   });
+  it('should accept values in the constructor', () => {
+    let dancer = new Dancer({
+      firstName: 'Maxime',
+      lastName: 'Gasnier',
+      age: 22,
+      danceStyles: ["Hip Hop", "BreakDance", "Zougoulou"],
+      points: 4
+    });
+    expect(dancer.firstName).toEqual('Maxime');
+    expect(dancer.lastName).toEqual('Gasnier');
+    expect(dancer.age).toEqual(22);
+    expect(dancer.danceStyles).toContain("Hip Hop");
+    expect(dancer.isNewMember).toEqual(false);
+    expect(dancer.points).toEqual(4);
+  });
 });
--- lkw-recruitment/src/app/class/dancer.ts
+++ lkw-recruitment/src/app/class/dancer.ts
@@ -1,2 +1,13 @@
 export class Dancer {
+    id: number;
+    firstName: string = '';
+    lastName: string = '';
+    age: number;
+    danceStyles: Array<string>;
+    isNewMember: boolean = false;
+    points: number = 0;
+
+    constructor(values: Object = {}) {
+        Object.assign(this, values);
+    }
 }
