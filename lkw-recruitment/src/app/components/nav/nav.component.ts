1cbe82e127c8e48acee0539e80023ba8803deafb
Simbadev <maxime.gasnier.1@gmail.com>
2017-07-22 20:41:32 +0200

    add active control to menu

--- lkw-recruitment/src/app/components/nav/nav.component.ts
+++ lkw-recruitment/src/app/components/nav/nav.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
+  menuActive: number = 2;
 
   constructor() { }
 
   ngOnInit() {
+  
   }
 
 }
